b759bf94ae84b1e7bd14bcbb88e410f433895cfe
TT <48351120+Tugbaturkekul@users.noreply.github.com>
2023-04-07 07:42:38 +0300

    video 67 ye kadar

---
+++ MVC_ProjeKamp/Controllers/MessageController.cs
@@ -0,0 +1,67 @@
+﻿using BusinessLayer.Concrete;
+using BusinessLayer.ValidationsRules;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using FluentValidation.Results;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_ProjeKamp.Controllers
+{
+    public class MessageController : Controller
+    {
+        MessageManager mm= new MessageManager(new EfMessageDal());
+        MessageValidator messageValidator = new MessageValidator();
+
+        public ActionResult Inbox()
+        {
+
+            var messagelist = mm.GetListInbox();
+            return View(messagelist);
+        }
+        public ActionResult Sendbox()
+        {
+            var messagelist = mm.GetListSendbox();
+            return View(messagelist);
+        }
+
+        public ActionResult GetInboxMessageDetail(int id)
+        {
+            var values = mm.GetByID(id);
+            return View(values);
+        }
+        public ActionResult GetSendboxMessageDetail(int id)
+        {
+            var values = mm.GetByID(id);
+            return View(values);
+        }
+        [HttpGet]
+        public ActionResult NewMessage()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult NewMessage(Message p)
+        {
+            ValidationResult results = messageValidator.Validate(p);
+            if (results.IsValid)
+            {
+                p.MessageDate = DateTime.Parse(DateTime.Now.ToShortDateString());
+                mm.MessageAddBL(p);
+                return RedirectToAction("Sendbox");
+
+            }
+            else
+            {
+                foreach (var item in results.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+            }
+            return View();
+        }
+    }
+}
