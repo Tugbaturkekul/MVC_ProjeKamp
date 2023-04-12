eb6a3a9d2bc18b525de9ebab73ca3b3c1c3fcfb5
TT <48351120+Tugbaturkekul@users.noreply.github.com>
2023-04-12 08:03:11 +0300

    Writer Login ok

--- MVC_ProjeKamp/Controllers/LoginController.cs
+++ MVC_ProjeKamp/Controllers/LoginController.cs
@@ -23,7 +23,7 @@ namespace MVC_ProjeKamp.Controllers
             var adminuserinfo = c.Admins.FirstOrDefault(x => x.AdminUserName == p.AdminUserName);
             if (adminuserinfo != null && adminuserinfo.AdminPassword == (p.AdminPassword))
             {
-                FormsAuthentication.SetAuthCookie(adminuserinfo.AdminUserName,false);
+                FormsAuthentication.SetAuthCookie(adminuserinfo.AdminUserName, false);
                 Session["AdminUserName"] = adminuserinfo.AdminUserName;
                 return RedirectToAction("Index", "AdminCategory");
             }
@@ -32,8 +32,33 @@ namespace MVC_ProjeKamp.Controllers
                 TempData["ErrorMessage"] = "Hatalı Kullanıcı Adı veya Şifre Girdiniz";
                 return RedirectToAction("Index");
             }
-           
+
         }
+        [HttpGet]
+        public ActionResult WriterLogin()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult WriterLogin(Writer p)
+        {
+            Context c = new Context();
+            var writeruserinfo = c.Writers.FirstOrDefault(x => x.WriterMail == p.WriterMail);
+            if (writeruserinfo != null && writeruserinfo.WriterPassword== (p.WriterPassword))
+            {
+                FormsAuthentication.SetAuthCookie(writeruserinfo.WriterMail, false);
+                Session["WriterMail"] = writeruserinfo.WriterMail;
+                return RedirectToAction("MyContent", "WriterPanelContent");
+            }
+            else
+            {
+                TempData["ErrorMessage"] = "Hatalı Kullanıcı Adı veya Şifre Girdiniz";
+                return RedirectToAction("WriterLogin");
+            }
+        
+        }
+
     }
 }
-    
+
+
--- MVC_ProjeKamp/Controllers/WriterPanelContentController.cs
+++ MVC_ProjeKamp/Controllers/WriterPanelContentController.cs
@@ -1,4 +1,5 @@
 ﻿using BusinessLayer.Concrete;
+using DataAccessLayer.Concrete;
 using DataAccessLayer.EntityFramework;
 using System;
 using System.Collections.Generic;
@@ -12,9 +13,14 @@ namespace MVC_ProjeKamp.Controllers
     public class WriterPanelContentController : Controller
     {
         ContentManager cm = new ContentManager(new EfContentDal());
-        public ActionResult MyContent()
+        public ActionResult MyContent(string p)
         {
-            var contentvalues = cm.GetListByWriter();
+            Context c = new Context();
+            p = (string)Session["WriterMail"];
+            //writeridinfo yu mimariye taşı babee
+            var writeridinfo = c.Writers.Where(x => x.WriterMail == p).Select(y => y.WriterID).FirstOrDefault();
+          //  ViewBag.d = p;
+            var contentvalues = cm.GetListByWriter(writeridinfo);
             return View(contentvalues);
         }
     }
